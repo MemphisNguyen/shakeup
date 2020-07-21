08bf8bd80da4db60cd28cb0b9056d93e0e325289
Nam Anh Nguyen <namnguyen.cs13@gmail.com>
2020-07-22 00:32:41 +0700

    Update cancel icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shake_v1.3.3",
+const CACHE_NAME = "shake_v1.3.4",
     FILES_TO_CACHE = ["/", "/manifest.json", "/assets/main.js", "/assets/main.min.css", "/assets/vue.min.js"];
 self.addEventListener("install", function (e) {
     self.skipWaiting();
